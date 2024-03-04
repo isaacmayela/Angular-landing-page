1e7466f87cc017f3aaea1ae9a4d030aaee84df1d
isaacmayela <isaacmayela27@gmail.com>
2024-03-04 18:00:22 +0100

    feat add landing-header componant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingHeaderComponent } from './landing-header/landing-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
